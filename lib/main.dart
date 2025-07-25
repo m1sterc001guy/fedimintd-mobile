bf8ad5c37a3d0edf49468f146a3b419deed99810
Justin <jumoell@protonmail.com>
2025-07-25 10:18:08 -0500

    add android webview

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:carbine/frb_generated.dart';
 import 'package:carbine/lib.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await RustLib.init();
-  _fedimintd();
+  await _fedimintd();
   runApp(const MyApp());
 }
 
@@ -24,13 +25,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello, world!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: WebViewScreen());
   }
 }
 
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition for Android (optional, improves WebView stability)
+    // WebView.platform = SurfaceAndroidWebView(); // Uncomment if needed and import
+
+    _controller =
+        WebViewController()
+          ..loadRequest(Uri.parse('http://localhost:8175'))
+          ..setJavaScriptMode(JavaScriptMode.unrestricted);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: WebViewWidget(controller: _controller));
+  }
+}
